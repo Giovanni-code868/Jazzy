4c63d38b2a5e540f32e1ea97fd19f10f22b1c095
Enemodia Giovanni <edafeenemodia@gmail.com>
2024-04-19 22:57:11 +0100

    work abeg

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -1,17 +1,24 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
 
 const Bag = () => {
-  const history = useHistory();
+  const [showModal, setShowModal] = useState(true); 
 
-  const handleProceedToCheckout = () => {
-  
-  };
+  const handleCloseModal = () => setShowModal(false);
+
+  // Close the modal after a certain time (e.g., 3 seconds)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      handleCloseModal();
+    }, 9000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <Modal show={true} centered>
+      {/* Modal */}
+      <Modal show={showModal} onHide={handleCloseModal} centered>
         <Modal.Header closeButton>
           <Modal.Title>Your Order</Modal.Title>
         </Modal.Header>
@@ -20,12 +27,12 @@ const Bag = () => {
             <p style={{ fontWeight: "bold" }}>Items:</p>
             <hr />
             <p style={{ color: "red" }}>1. Double beef burger</p>
-            {/* Add more items here */}
+            
             <hr />
             <p>Item subtotal: $10</p>
             {/* Add subtotal calculation here */}
             <hr />
-            <Button variant="success" className="w-100" onClick={handleProceedToCheckout}>
+            <Button variant="success" className="w-100" onClick={handleCloseModal}>
               Proceed to Checkout
             </Button>
           </div>
